52d25b1de288512acb88337b67612fe9b7da07f6
Sergei Yarema <isara.isara8@gmail.com>
2019-09-20 23:57:23 +0300

    Add spos lib

--- MultitaskingManager/src/com/university/Main.java
+++ MultitaskingManager/src/com/university/Main.java
@@ -1,8 +1,14 @@
 package com.university;
 
+import spos.lab1.demo.DoubleOps;
+import spos.lab1.demo.IntOps;
+import spos.lab1.demo.Conjunction;
+import spos.lab1.demo.Disjunction;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("dsd");
     }
 }
